0b44149c829287005c17042d6784226e27d8bc62
ctrysbita <ctrysbita@users.noreply.github.com>
2020-08-19 17:22:43 +0000

    deploy: XMUMY/XMUX@98d513d539a44cff25293c05340e07099580c0d5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,42 +3,42 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "d2131a8abe527949b2e5bb277cdd0238",
+  "icons/Icon-192.png": "6b4ea9f6031012c905a82e82f82d6091",
+"icons/Icon-512.png": "67e8216384de07e96cf42829946d5aa7",
 "favicon.png": "5a218aa39cebdb43f68a3fd691e200b0",
+"main.dart.js": "9909bffafac03f4a9a365bba4df43fbd",
 "index.html": "8a5722937b6690ed6e6f8c44813d3328",
 "/": "8a5722937b6690ed6e6f8c44813d3328",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/FontManifest.json": "ef6e61413ebb7197d1a41677d9fd4515",
-"assets/res/campus/maintenance.svg": "b9cae80e08be80fa21cb423efeef245c",
-"assets/res/campus/vpn.svg": "beefc182629a78014647916b058e7d64",
-"assets/res/campus/ecr.svg": "297f82fe4f4b56d11582387d21171564",
-"assets/res/campus/travelviser.svg": "f257d1ed9975ddc9bd9315e6667b9b7e",
-"assets/res/campus/klia_express.svg": "cd072c9ebddb012739680611c6d6a737",
-"assets/res/campus/gpa.svg": "4efff844345ad45d7d2fc1a7b0eada2e",
-"assets/res/campus/gpa_calculator.svg": "4a1b55cbf512e6454ab6edd9300416ce",
-"assets/res/campus/wolfram.svg": "c34be2eb5890419d5e38fc2f958bb545",
-"assets/res/campus/geogebra.svg": "dfc4705883c2c8655c045b98479c386f",
+"manifest.json": "1a086f325edce20cb619b19e7fdc4474",
+"assets/FontManifest.json": "14a380732d4980463a62c35483c5a669",
+"assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
 "assets/res/static.json": "441d5606bd8b6dd914143c0fbc4b16e1",
+"assets/res/logo.png": "99a0993313ea5fb8b85758d6a9469340",
 "assets/res/initpage.jpg": "99ad733b153432601e06df23fc5a7795",
+"assets/res/animations/vpn.json": "731e1f93c663e7738c4e57c1d8010a76",
+"assets/res/animations/stars.flr": "3f714e8b368130e65bdbca7b624a1cfd",
+"assets/res/home/news.svg": "7a2a6edd152ffe5461bbe998c99844ee",
 "assets/res/translations/en.json": "89e9e5df4fe9c349031bcfdbe5b2fd6a",
 "assets/res/translations/zh-CN.json": "6edfaf9ce7af523f28ae80d40712d3cd",
-"assets/res/logo.png": "99a0993313ea5fb8b85758d6a9469340",
-"assets/res/animations/stars.flr": "3f714e8b368130e65bdbca7b624a1cfd",
-"assets/res/animations/vpn.json": "731e1f93c663e7738c4e57c1d8010a76",
-"assets/res/home/news.svg": "7a2a6edd152ffe5461bbe998c99844ee",
+"assets/res/campus/gpa.svg": "4efff844345ad45d7d2fc1a7b0eada2e",
+"assets/res/campus/wolfram.svg": "c34be2eb5890419d5e38fc2f958bb545",
+"assets/res/campus/ecr.svg": "297f82fe4f4b56d11582387d21171564",
+"assets/res/campus/klia_express.svg": "cd072c9ebddb012739680611c6d6a737",
+"assets/res/campus/travelviser.svg": "f257d1ed9975ddc9bd9315e6667b9b7e",
+"assets/res/campus/gpa_calculator.svg": "4a1b55cbf512e6454ab6edd9300416ce",
+"assets/res/campus/vpn.svg": "beefc182629a78014647916b058e7d64",
+"assets/res/campus/maintenance.svg": "b9cae80e08be80fa21cb423efeef245c",
+"assets/res/campus/geogebra.svg": "dfc4705883c2c8655c045b98479c386f",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
-"assets/packages/easy_localization/i18n/en-US.json": "5bd908341879a431441c8208ae30e4fd",
-"assets/packages/easy_localization/i18n/ar-DZ.json": "acc0a8eebb2fcee312764600f7cc41ec",
-"assets/packages/easy_localization/i18n/ar.json": "acc0a8eebb2fcee312764600f7cc41ec",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "assets/packages/easy_localization/i18n/en.json": "5bd908341879a431441c8208ae30e4fd",
+"assets/packages/easy_localization/i18n/ar-DZ.json": "acc0a8eebb2fcee312764600f7cc41ec",
+"assets/packages/easy_localization/i18n/en-US.json": "5bd908341879a431441c8208ae30e4fd",
+"assets/packages/easy_localization/i18n/ar.json": "acc0a8eebb2fcee312764600f7cc41ec",
 "assets/packages/open_iconic_flutter/assets/open-iconic.woff": "3cf97837524dd7445e9d1462e3c4afe2",
-"assets/NOTICES": "b7a1655377c8bffacd4b219f3a85b993",
 "assets/AssetManifest.json": "1992c9b68fb4f18c0fb2a03caec70ea6",
-"manifest.json": "1a086f325edce20cb619b19e7fdc4474",
-"icons/Icon-512.png": "67e8216384de07e96cf42829946d5aa7",
-"icons/Icon-192.png": "6b4ea9f6031012c905a82e82f82d6091"
+"assets/NOTICES": "37ff0d426d03cd38af99d1a7d8f9cf48"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -55,8 +55,8 @@ const CORE = [
 self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
-      // Provide a no-cache param to ensure the latest version is downloaded.
-      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'no-cache'})));
+      // Provide a 'reload' param to ensure the latest version is downloaded.
+      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
@@ -139,7 +139,7 @@ self.addEventListener("fetch", (event) => {
         // Either respond with the cached resource, or perform a fetch and
         // lazily populate the cache. Ensure the resources are not cached
         // by the browser for longer than the service worker expects.
-        var modifiedRequest = new Request(event.request, {'cache': 'no-cache'});
+        var modifiedRequest = new Request(event.request, {'cache': 'reload'});
         return response || fetch(modifiedRequest).then((response) => {
           cache.put(event.request, response.clone());
           return response;
