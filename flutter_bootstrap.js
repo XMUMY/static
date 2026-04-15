775691d6b96833727963abb459b9995674d985be
ctrysbita <ctrysbita@users.noreply.github.com>
2026-04-15 16:26:47 +0000

    deploy: XMUMY/XMUX@4727d546386d4112936522288ef83c68800e0227

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"1d35cccf5e47d19947a669000a53a26cabb5af5e","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"4f3679cadc3804a16abfe00f51146f4291f9384c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2724615401" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2099140803" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
